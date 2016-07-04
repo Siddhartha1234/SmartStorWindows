4f188c65b76cddd3ef4e2f125ee24e76eb7baeef
Siddhartha1234 <es15btech11018@iith.ac.in>
2016-07-04 17:19:59 +0530

    music file test

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -43,19 +43,19 @@
             this.button1 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
-            this.button4 = new System.Windows.Forms.Button();
+            this.buttonShareMusic = new System.Windows.Forms.Button();
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             this.tableLayoutPanel4.SuspendLayout();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
-            this.tableLayoutPanel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tableLayoutPanel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -102,6 +102,7 @@
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
             this.tableLayoutPanel2.Size = new System.Drawing.Size(205, 514);
             this.tableLayoutPanel2.TabIndex = 1;
+            this.tableLayoutPanel2.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel2_Paint);
             // 
             // buttonenable
             // 
@@ -168,7 +169,7 @@
             this.tableLayoutPanel4.ColumnCount = 3;
             this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 47.40741F));
             this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 52.59259F));
-            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 71F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 73F));
             this.tableLayoutPanel4.Controls.Add(this.button2, 0, 0);
             this.tableLayoutPanel4.Controls.Add(this.button3, 0, 0);
             this.tableLayoutPanel4.Controls.Add(this.button1, 0, 0);
@@ -182,7 +183,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(131, 3);
+            this.button2.Location = new System.Drawing.Point(129, 3);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(59, 52);
             this.button2.TabIndex = 4;
@@ -193,18 +194,19 @@
             // button3
             // 
             this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
-            this.button3.Location = new System.Drawing.Point(64, 3);
+            this.button3.Location = new System.Drawing.Point(63, 3);
             this.button3.Name = "button3";
             this.button3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.button3.Size = new System.Drawing.Size(61, 52);
+            this.button3.Size = new System.Drawing.Size(60, 52);
             this.button3.TabIndex = 5;
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(3, 3);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(55, 52);
+            this.button1.Size = new System.Drawing.Size(54, 52);
             this.button1.TabIndex = 3;
             this.button1.Text = "<<";
             this.button1.UseVisualStyleBackColor = true;
@@ -219,17 +221,29 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(810, 514);
             this.panel1.TabIndex = 4;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // panel2
             // 
             this.panel2.Controls.Add(this.dataGridView1);
             this.panel2.Controls.Add(this.label1);
-            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.buttonShareMusic);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel2.Location = new System.Drawing.Point(0, 0);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(810, 514);
             this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 80);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(807, 457);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // label1
             // 
@@ -241,15 +255,15 @@
             this.label1.Text = "label1";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // button4
+            // buttonShareMusic
             // 
-            this.button4.Location = new System.Drawing.Point(288, 27);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(250, 45);
-            this.button4.TabIndex = 0;
-            this.button4.Text = "Share to connected device";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            this.buttonShareMusic.Location = new System.Drawing.Point(288, 27);
+            this.buttonShareMusic.Name = "buttonShareMusic";
+            this.buttonShareMusic.Size = new System.Drawing.Size(250, 45);
+            this.buttonShareMusic.TabIndex = 0;
+            this.buttonShareMusic.Text = "Share to connected device";
+            this.buttonShareMusic.UseVisualStyleBackColor = true;
+            this.buttonShareMusic.Click += new System.EventHandler(this.button4_Click_1);
             // 
             // webBrowser1
             // 
@@ -283,15 +297,6 @@
             this.progressBar1.TabIndex = 0;
             this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
             // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(3, 80);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(807, 457);
-            this.dataGridView1.TabIndex = 3;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -307,8 +312,8 @@
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
-            this.tableLayoutPanel3.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tableLayoutPanel3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -332,7 +337,7 @@
         private System.Windows.Forms.WebBrowser webBrowser1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button buttonShareMusic;
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.DataGridView dataGridView1;
     }
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -10,6 +10,11 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.Reflection;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+using System.Drawing.Imaging;
+using System.Web;
+using System.Security.AccessControl;
 
 namespace WindowsFormsApplication1
 {
@@ -21,7 +26,8 @@ namespace WindowsFormsApplication1
         public String[] dirs = Environment.GetLogicalDrives();       
         private BackgroundWorker bw = new BackgroundWorker();
         private BackgroundWorker mworker = new BackgroundWorker();
-
+        private BackgroundWorker sworker = new BackgroundWorker();
+        
        
         public Form1()
         {
@@ -34,7 +40,128 @@ namespace WindowsFormsApplication1
             mworker.DoWork += new DoWorkEventHandler(mworker_DoWork);
             mworker.ProgressChanged += new ProgressChangedEventHandler(mworker_ProgressChanged);
             mworker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(mworker_RunWorkerCompleted);
+            sworker.WorkerReportsProgress = true;
+            sworker.WorkerSupportsCancellation = true;
+            sworker.DoWork += new DoWorkEventHandler(sworker_DoWork);
+            sworker.ProgressChanged += new ProgressChangedEventHandler(sworker_ProgressChanged);
+            sworker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(sworker_RunWorkerCompleted);
 
+
+        }
+        private delegate void myUpdate(int progress);
+
+        //method to update progress bar
+        private void updateProgress(int progress)
+        {
+            progressBar1.Value = progress;
+        }
+        private void sworker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            string path = @"c:\\Program Files\\server-nodejs\\musicJSON.json";
+            File.Delete(path);
+            File.Create(path).Close();
+            Music music = new Music();
+            music.music = new List<MusicMetadata>();
+            using (FileStream fs1 = new FileStream(@"c:\\Program Files\\server-nodejs\\musiclist.txt", FileMode.Open, FileAccess.Read))
+            using (StreamReader sr = new StreamReader(fs1))
+            {
+                int count = File.ReadLines(@"c:\\Program Files\\server-nodejs\\musiclist.txt").Count();
+                int c1 = 0;
+                while (!sr.EndOfStream)
+                {
+                    if (File.Exists(sr.ReadLine()))
+                    {
+                        Invoke(new myUpdate(updateProgress), ((int)((++c1) *2* 100) / (count)));
+                        string @pth = sr.ReadLine();
+                        if (pth == null)
+                            continue;
+                        //GrantAccess(pth);
+                        TagLib.File file;
+                        try
+                        {
+                            file = TagLib.File.Create(pth);
+                        }
+                        catch(TagLib.CorruptFileException err)
+                        {
+                            continue;
+                        }
+                        if (file.Tag.Title != null)
+                        {
+                            if (file.Tag.Title.Length > 0)
+                            {
+                                bool flag = false;
+                                foreach (char c in System.IO.Path.GetInvalidFileNameChars())
+                                {
+                                    if (file.Tag.Title.Contains(c)) { flag = true; break; }
+                                   
+
+                                }
+                                if (flag)
+                                    continue;
+                                
+
+                                if (file.Tag.Pictures.Length > 0)
+                                    {
+                                        byte[] bitmap = file.Tag.Pictures[0].Data.Data;
+                                        if (bitmap.Length > 0)
+                                        {
+                                            using (Image image = Image.FromStream(new MemoryStream(bitmap)))
+                                            {
+                                                image.Save(@"c:\\Program Files\\server-nodejs\\pictures\\" + file.Tag.Title + ".jpg", ImageFormat.Jpeg);  // Or Png
+                                            }
+                                        }
+                                    }
+                                    MusicMetadata mm = new MusicMetadata(file.Tag.Title, file.Tag.Album, file.Tag.FirstPerformer, file.Tag.FirstGenre,"stream-music?path="+pth, @"get-picture?path=c:\\Program Files\\server-nodejs\\pictures\\" + file.Tag.Title + ".jpg", file.Tag.Track, file.Tag.TrackCount, file.Properties.Duration.TotalSeconds, file.Properties.Description);
+                                    if (mm != null)
+                                        music.music.Add(mm);
+                               
+
+                                }
+                            }
+                        }
+                    }
+                }
+                string output = JsonConvert.SerializeObject(music, Formatting.Indented);
+
+                using (FileStream fs = new FileStream(path, FileMode.Append, FileAccess.Write))
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    sw.WriteLine(output);
+                }
+            }
+        
+        private bool GrantAccess(string fullPath)
+        {
+            DirectoryInfo dInfo = new DirectoryInfo(fullPath);
+            DirectorySecurity dSecurity = dInfo.GetAccessControl();
+            dSecurity.AddAccessRule(new FileSystemAccessRule("everyone", FileSystemRights.FullControl,
+                                                             InheritanceFlags.ObjectInherit | InheritanceFlags.ContainerInherit,
+                                                             PropagationFlags.NoPropagateInherit, AccessControlType.Allow));
+            dInfo.SetAccessControl(dSecurity);
+            return true;
+        }
+        private void sworker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if ((e.Cancelled == true))
+            {
+                this.progressBar1.Text = "Canceled!";
+            }
+
+            else if (!(e.Error == null))
+            {
+                MessageBox.Show("Error Loading Virtual File ", e.Error.ToString());
+            }
+
+            else
+            {
+                MessageBox.Show("Click on Music in another device to stream the Music files in this device!", "Virtual File generated");
+            }
+        }
+
+        private void sworker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            this.progressBar1.Value = (e.ProgressPercentage);
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -128,7 +255,7 @@ namespace WindowsFormsApplication1
                             {
                                  data.Add(sr.ReadLine());
                                  dataGridView1.Rows.Add(data.Last());
-
+                                    
                                 
                             }
 
@@ -140,17 +267,18 @@ namespace WindowsFormsApplication1
                 bw.CancelAsync();
                 bw.Dispose();
                 string path = @"c:\\Program Files\\server-nodejs\\musiclist.txt";
-                using (FileStream fs = new FileStream(path , FileMode.CreateNew, FileAccess.ReadWrite))
+                using (FileStream fs = new FileStream(path, FileMode.CreateNew, FileAccess.ReadWrite))
                 using (StreamWriter sw = new StreamWriter(fs))
                 {
                     for (int i = 0; i < data.Count; i++)
                     {
-                        if (File.Exists(data.ElementAt(i)))
-                            sw.WriteLine(data.ElementAt(i));
+                            if (File.Exists(data.ElementAt(i)))
+                                    sw.WriteLine(data.ElementAt(i));
                     }
                     sw.Close();
 
                 }
+
                 if (!bw.IsBusy)
                     bw.RunWorkerAsync();
              }
@@ -175,6 +303,7 @@ namespace WindowsFormsApplication1
             p.StartInfo.FileName = @"c:\\Program Files\\server-nodejs\\npm.cmd";
             p.StartInfo.WorkingDirectory = @"c:\\Program Files\\server-nodejs";
             p.StartInfo.Arguments = @"start";
+            p.StartInfo.RedirectStandardOutput = false;
            // p.StartInfo.FileName = "cmd.exe";
             p.StartInfo.CreateNoWindow = true;
             p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
@@ -220,7 +349,12 @@ namespace WindowsFormsApplication1
 
         private void button4_Click_1(object sender, EventArgs e)
         {
-
+            if (!sworker.IsBusy)
+            {
+                
+                sworker.RunWorkerAsync();
+                MessageBox.Show("Generating virtual file to share!");
+            }
         }
 
         private void buttonmusiclib_Click(object sender, EventArgs e)
@@ -241,7 +375,7 @@ namespace WindowsFormsApplication1
                         {
                             data.Add(sr.ReadLine());
                             dataGridView1.Rows.Add(data.Last());
-
+                          
 
                         }
                     }
@@ -338,6 +472,72 @@ namespace WindowsFormsApplication1
         {
 
         }
+
+        private void tableLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+    public class Music {
+        public List<MusicMetadata> music;    
+    }
+    public class MusicMetadata
+    {
+        public string title;
+        public string album;
+        public string artist;
+        public string genre;
+        public string source;
+        public string image;
+        public uint trackNumber;
+        public uint totalTrackCount;
+        public double duration;
+        public string site;
+        public MusicMetadata(string title, string album,string artist,string genre,string source,string image,uint trackNumber,uint totalTrackCount,double duration,string site)
+        {
+            if (title != null)
+                this.title = title;
+            else
+                this.title = " ";
+
+            if (album != null)
+                this.album =album;
+            else
+                this.album = " ";
+            this.artist = artist;
+            if (genre != null)
+                this.genre = genre;
+            else
+                this.genre = " ";
+            this.source = source;
+            this.image = image;
+            this.trackNumber = trackNumber;
+            this.totalTrackCount = totalTrackCount;
+            this.duration = duration;
+            this.site = site;
+        }
+        
+
     }
     public sealed class Privilege : IDisposable
     {
